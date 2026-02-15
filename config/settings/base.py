b68738426ad42e649809d7bbc187dff01d1e589a
Job Etokabeka <jobet@Mac.lan>
2026-02-15 13:17:10 +0100

    Version du 15 février 2026

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,12 +80,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 600,
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
