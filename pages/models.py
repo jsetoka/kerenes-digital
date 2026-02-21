d9544357d2256cbfba615c2b7ced83a591870498
Job Etokabeka <jobet@Mac.lan>
2026-02-21 09:28:01 +0100

    21/02

--- pages/models.py
+++ pages/models.py
@@ -118,22 +118,12 @@ class FormationsIndexPage(Page):
         use_json_field=True,
         blank=True,
     )
-    body = StreamField(
-        [
-            ("paragraphe", blocks.RichTextBlock(features=[
-             "h2", "h3", "bold", "italic", "link", "ol", "ul"])),
-            ("image", ImageChooserBlock()),
-        ],
-        use_json_field=True,
-        blank=True,
-    )
     parent_page_types = ["pages.HomePage", "wagtailcore.Page"]  # ✅ au choix
     # ✅ seulement les formations dedans
     subpage_types = ["pages.FormationPage"]
 
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
-        FieldPanel("body"),
     ]
 
     def get_context(self, request):
